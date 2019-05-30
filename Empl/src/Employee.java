e8fd0af022237d64f0518c63d09dafeed0bd9107
ssriniminnie <srinivasbhats12@gmail.com>
2019-05-30 16:46:41 +0530

    tiger

---
+++ Empl/src/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee {
+ private int empid;
+ private String empname;
+ private double empSal;
+public int getEmpid() {
+	return empid;
+}
+public void setEmpid(int empid) {
+	this.empid = empid;
+}
+public String getEmpname() {
+	return empname;
+}
+public void setEmpname(String empname) {
+	this.empname = empname;
+}
+public double getEmpSal() {
+	return empSal;
+}
+public void setEmpSal(double empSal) {
+	this.empSal = empSal;
+}
+ 
+}
